9f6e676696897397afc836f473cd662319f1db36
netchar <e.glushankov@gmail.com>
2021-03-16 21:53:22 +0300

    * cleanup

--- app/src/main/java/com/netchar/nicknamer/presentation/main/MainFragment.kt
+++ app/src/main/java/com/netchar/nicknamer/presentation/main/MainFragment.kt
@@ -5,59 +5,52 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.netchar.nicknamer.R
 import com.netchar.nicknamer.databinding.FragmentMainBinding
-import com.netchar.nicknamer.domen.models.Config
 import com.netchar.nicknamer.presentation.copyToClipboard
 import com.netchar.nicknamer.presentation.viewBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
-    private val viewModel by viewModels<MainViewModel>() { MainViewModelFactory() }
+    private val viewModel by viewModels<MainViewModel> { MainViewModelFactory() }
     private val binding by viewBinding(FragmentMainBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        bind()
+        bindViews()
         observe()
     }
 
-    private fun bind() {
-        binding.mainTvNickname.setOnLongClickListener {
+    private fun bindViews() = with(binding) {
+        mainTvNickname.setOnClickListener {
             copyToClipboard()
-            true
         }
 
-        binding.mainBtnGenerate.setOnClickListener {
-            viewModel.generateNewNickname(Config(nicknameLength, gender, alphabet))
+        mainBtnGenerate.setOnClickListener {
+            viewModel.generateNewNickname()
         }
 
-        binding.mainBtnGenerate.performClick()
+        mainRadioGrpGender.setOnCheckedChangeListener { _, checkedId ->
+            viewModel.setGender(checkedId)
+        }
+
+        mainRadioGrpAlphabet.setOnCheckedChangeListener { _, checkedId ->
+            viewModel.setAlphabet(checkedId)
+        }
+
+        mainSlideLength.addOnChangeListener { _, value, _ ->
+            viewModel.setLength(value)
+        }
     }
 
     private fun observe() {
-        viewModel.nickname.observe(viewLifecycleOwner, Observer { nickname ->
+        viewModel.nickname.observe(viewLifecycleOwner, { nickname ->
             binding.mainTvNickname.text = nickname.value
         })
     }
 
-    private val nicknameLength: Int get() = binding.mainSlideLength.value.toInt()
-
-    private val gender: Config.Gender
-        get() = when (binding.mainRadioGrpGender.checkedRadioButtonId) {
-            R.id.main_radio_btn_male -> Config.Gender.MALE
-            else -> Config.Gender.FEMALE
-        }
-
-    private val alphabet: Config.Alphabet
-        get() = when (binding.mainRadioGrpAlphabet.checkedRadioButtonId) {
-            R.id.main_radio_btn_latin -> Config.Alphabet.LATIN
-            else -> Config.Alphabet.CYRILLIC
-        }
-
     private fun copyToClipboard() {
-        requireContext().copyToClipboard(binding.mainTvNickname.text)
+        val context = requireContext()
+        context.copyToClipboard(binding.mainTvNickname.text)
         Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/netchar/nicknamer/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/netchar/nicknamer/presentation/main/MainViewModel.kt
@@ -4,21 +4,53 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.netchar.nicknamer.R
 import com.netchar.nicknamer.domen.models.Config
 import com.netchar.nicknamer.domen.models.Nickname
 import com.netchar.nicknamer.domen.service.NicknameGeneratorService
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MainViewModel(
         private val nicknameService: NicknameGeneratorService
 ) : ViewModel() {
     private val mutableNickname = MutableLiveData<Nickname>()
+    private var gender: Config.Gender = Config.Gender.MALE
+    private var alphabet: Config.Alphabet = Config.Alphabet.LATIN
+    private var nicknameLength: Float = 5.0f
+    private var job: Job? = null
 
     val nickname: LiveData<Nickname> = mutableNickname
 
-    fun generateNewNickname(config: Config) {
-        viewModelScope.launch {
-            mutableNickname.value = nicknameService.generateNickname(config)
+    init {
+        generateNewNickname()
+    }
+
+    fun generateNewNickname() {
+        if (job?.isActive == true) {
+            return
         }
+
+        job = viewModelScope.launch {
+            mutableNickname.value = nicknameService.generateNickname(Config(nicknameLength.toInt(), gender, alphabet))
+        }
+    }
+
+    fun setGender(checkedId: Int) {
+        gender = when (checkedId) {
+            R.id.main_radio_btn_male -> Config.Gender.MALE
+            else -> Config.Gender.FEMALE
+        }
+    }
+
+    fun setAlphabet(checkedId: Int) {
+        alphabet = when (checkedId) {
+            R.id.main_radio_btn_latin -> Config.Alphabet.LATIN
+            else -> Config.Alphabet.CYRILLIC
+        }
+    }
+
+    fun setLength(length: Float) {
+        nicknameLength = length
     }
 }
